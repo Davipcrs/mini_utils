8fc7d51c0dd016cf74611f51964ca2bae0233d11
Davipcrs <davi.coelho_comp@outlook.com>
2024-02-28 09:39:20 -0300

    SetState in the color_picker for container Preview

--- lib/src/color_picker.dart
+++ lib/src/color_picker.dart
@@ -8,95 +8,144 @@ Future<Color?> showColorPicker({
   int red = 0;
   int green = 0;
   int blue = 0;
-  int opacity = 0;
+  int opacity = 255;
   Color? selectedColor = Color.fromARGB(opacity, red, green, blue);
   TextEditingController redController = TextEditingController();
   TextEditingController greenController = TextEditingController();
   TextEditingController blueController = TextEditingController();
   TextEditingController opacityController = TextEditingController();
+  redController.text = red.toString();
+  greenController.text = green.toString();
+  blueController.text = blue.toString();
+  opacityController.text = opacity.toString();
 
   await showAdaptiveDialog(
     context: context,
     builder: (context) {
-      return Padding(
-        padding: const EdgeInsets.all(12.0),
-        child: SizedBox(
-          width: MediaQuery.of(context).size.width * 0.60,
-          height: MediaQuery.of(context).size.height * 0.60,
-          child: Scaffold(
-            body: Column(
-              children: [
-                Row(
+      return StatefulBuilder(builder: (context, state) {
+        return Dialog(
+          child: Padding(
+            padding: const EdgeInsets.all(12.0),
+            child: SizedBox(
+              width: MediaQuery.of(context).size.width * 0.60,
+              height: MediaQuery.of(context).size.height * 0.60,
+              child: Scaffold(
+                body: Column(
                   children: [
-                    Container(
-                      decoration: BoxDecoration(color: selectedColor),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.25,
+                          height: MediaQuery.of(context).size.height * 0.60,
+                          child: Container(
+                            decoration: BoxDecoration(color: selectedColor),
+                          ),
+                        ),
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width * 0.25,
+                          height: MediaQuery.of(context).size.height * 0.60,
+                          child: Column(
+                            children: [
+                              //DropdownMenu(dropdownMenuEntries: dropdownMenuEntries),
+                              TextField(
+                                controller: redController,
+                                keyboardType: TextInputType.number,
+                                inputFormatters: [
+                                  FilteringTextInputFormatter.digitsOnly
+                                ],
+                                onEditingComplete: () {
+                                  state(
+                                    () {
+                                      red = int.parse(redController.text);
+                                      selectedColor = Color.fromARGB(
+                                          opacity, red, green, blue);
+                                    },
+                                  );
+                                },
+                              ), // R
+                              TextField(
+                                controller: greenController,
+                                keyboardType: TextInputType.number,
+                                inputFormatters: [
+                                  FilteringTextInputFormatter.digitsOnly
+                                ],
+                                onEditingComplete: () {
+                                  state(
+                                    () {
+                                      green = int.parse(redController.text);
+                                      selectedColor = Color.fromARGB(
+                                          opacity, red, green, blue);
+                                    },
+                                  );
+                                },
+                              ), // G
+                              TextField(
+                                controller: blueController,
+                                keyboardType: TextInputType.number,
+                                inputFormatters: [
+                                  FilteringTextInputFormatter.digitsOnly
+                                ],
+                                onEditingComplete: () {
+                                  state(
+                                    () {
+                                      blue = int.parse(redController.text);
+                                      selectedColor = Color.fromARGB(
+                                          opacity, red, green, blue);
+                                    },
+                                  );
+                                },
+                              ), // B
+                              TextField(
+                                controller: opacityController,
+                                keyboardType: TextInputType.number,
+                                inputFormatters: [
+                                  FilteringTextInputFormatter.digitsOnly
+                                ],
+                                onEditingComplete: () {
+                                  state(
+                                    () {
+                                      opacity = int.parse(redController.text);
+                                      selectedColor = Color.fromARGB(
+                                          opacity, red, green, blue);
+                                    },
+                                  );
+                                },
+                              ), // O
+                            ],
+                          ),
+                        )
+                      ],
                     ),
-                    SizedBox(
-                      width: MediaQuery.of(context).size.width * 0.30,
-                      height: MediaQuery.of(context).size.height * 0.30,
-                      child: Column(
-                        children: [
-                          //DropdownMenu(dropdownMenuEntries: dropdownMenuEntries),
-                          TextField(
-                            controller: redController,
-                            keyboardType: TextInputType.number,
-                            inputFormatters: [
-                              FilteringTextInputFormatter.digitsOnly
-                            ],
-                          ), // R
-                          TextField(
-                            controller: greenController,
-                            keyboardType: TextInputType.number,
-                            inputFormatters: [
-                              FilteringTextInputFormatter.digitsOnly
-                            ],
-                          ), // G
-                          TextField(
-                            controller: blueController,
-                            keyboardType: TextInputType.number,
-                            inputFormatters: [
-                              FilteringTextInputFormatter.digitsOnly
-                            ],
-                          ), // B
-                          TextField(
-                            controller: opacityController,
-                            keyboardType: TextInputType.number,
-                            inputFormatters: [
-                              FilteringTextInputFormatter.digitsOnly
-                            ],
-                          ), // O
-                        ],
-                      ),
-                    )
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        ElevatedButton(
+                          onPressed: () async {
+                            Navigator.of(context).pop();
+                            selectedColor = null;
+                            return;
+                          },
+                          child: Text("Cancel"),
+                        ),
+                        ElevatedButton(
+                          onPressed: () async {
+                            selectedColor =
+                                Color.fromARGB(opacity, red, green, blue);
+                            Navigator.of(context).pop();
+                            return;
+                          },
+                          child: Text("Confirm"),
+                        ),
+                      ],
+                    ),
                   ],
                 ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    ElevatedButton(
-                      onPressed: () async {
-                        Navigator.of(context).pop();
-                        selectedColor = null;
-                        return;
-                      },
-                      child: Text("Cancel"),
-                    ),
-                    ElevatedButton(
-                      onPressed: () async {
-                        selectedColor =
-                            Color.fromARGB(opacity, red, green, blue);
-                        Navigator.of(context).pop();
-                        return;
-                      },
-                      child: Text("Confirm"),
-                    ),
-                  ],
-                ),
-              ],
+              ),
             ),
           ),
-        ),
-      );
+        );
+      });
     },
   );
   return selectedColor;
