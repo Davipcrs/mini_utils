23c3b234e618745b33a092f924c08b9569d188cd
Davipcrs <davi.coelho_comp@outlook.com>
2024-02-29 17:00:22 -0300

    init v0.0.3

--- lib/mini_utils.dart
+++ lib/mini_utils.dart
@@ -2,3 +2,5 @@ library mini_utils;
 
 export 'src/date_time_picker.dart' show showDateTimePicker;
 export 'src/color_picker.dart' show showColorPicker;
+export 'src/login_screen.dart' show loginScreen;
+export 'src/digital_clock.dart' show digitalClock;
